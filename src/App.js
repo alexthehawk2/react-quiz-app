b8241610cda48a92640717de5c87b346cdecab51
alexthehawk <abirdey773@gmail.com>
2023-01-01 19:16:53 +0530

    logic done loader left

--- src/App.js
+++ src/App.js
@@ -2,30 +2,108 @@ import "./style.css";
 import StartScreen from "./components/StartScreen";
 import { useEffect, useState } from "react";
 import Quiz from "./components/Quiz";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
 function App() {
   const [quizData, setQuizData] = useState(false);
-  const [startStatus, setStartStatus] = useState(true);
+  const [startStatus, setStartStatus] = useState(false);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [quizEnd, setQuizEnd] = useState(false)
+  const [startAgain, setStartAgain] = useState(false)
+  const apiEndpoint =
+        "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy";
   function start() {
     const startElement = document.getElementsByClassName("entry-container")[0];
     startElement.style.display = "none";
     setStartStatus(true);
+    setStartAgain(true)
+  }
+  function allValuesTrue(array) {
+    return array.every(val => val === true);
+  }
+  function checkAnswers() {
+    
+    
+    const allAnswersSelected = quizData.map(question=> question.selectedAnswer ? true : false)
+    if(allValuesTrue(allAnswersSelected)){
+      setQuizEnd(true)
+      setStartStatus(false)
+      setQuizData(oldQuizData => oldQuizData.map(question=>{
+        if(question.correct_answer === question.selectedAnswer){
+          setCorrectCount(oldCorrectCount => oldCorrectCount + 1)
+          return {...question, rightAnswerSelected:true}
+        }else {
+          return question
+        }
+      }))
+    }else {
+      
+
+    }
+  }
+  function selectAnswer(event){
+    const clickedButtonElement  = event.target
+    const questionId = event.target.id
+    const selectedAnswerValue = event.target.innerHTML
+    if(clickedButtonElement.classList[1]){
+
+      setQuizData(oldQuizData=> oldQuizData.map(question =>{
+        return question.questionId === questionId ? {...question, selectedAnswer: false} :
+         question
+       }))
+    }else{
+      setQuizData(oldQuizData=> oldQuizData.map(question =>{
+       return question.questionId === questionId ? {...question, selectedAnswer: selectedAnswerValue} :
+        question
+      }))
+    }
+  }
+
+  function playAgain(){
+
+    setCorrectCount(0)
+    setQuizEnd(false)
+    setQuizData(false)
+    setStartStatus(true)
+    setStartAgain(startAgain => !startAgain)
+
+    
+  }
+  function decodeAllArrayItems(arr){
+    const newArr = []
+    arr.forEach(item=> newArr.push(decode(item)))
+    return newArr
   }
   useEffect(() => {
-    if (startStatus) {
-      const apiEndpoint =
-        "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy";
+     if(!startStatus){
+
+     }else{
       fetch(apiEndpoint)
         .then((res) => res.json())
         .then((data) => {
-          setQuizData(data.results);
+          setQuizData(data.results.map(question=> {
+            const randomAnswerPlacement = question.incorrect_answers.concat(question.correct_answer)
+            randomAnswerPlacement.splice(Math.floor(Math.random() * randomAnswerPlacement.length), 0, randomAnswerPlacement.pop());
+
+            return (
+              {...question, selectedAnswer : false , questionId : nanoid(), rightAnswerSelected : false, randomAnswerPlacement: decodeAllArrayItems(randomAnswerPlacement), question: decode(question.question), incorrect_answers: decodeAllArrayItems(question.incorrect_answers),correct_answer: decode(question.correct_answer)}
+            )
+          }));
         });
-    }
-  }, [startStatus]);
+     }
+      
+    
+  }, [startStatus,startAgain]);
   return (
     <div className="App">
-      {/* <StartScreen start={start} /> */}
-      {quizData && <Quiz quizData={quizData} />}
-      <button className="start-btn check-btn">Check Answer</button>
+      <StartScreen start={start} />
+      {quizData && <Quiz selectAnswer={selectAnswer} startStatus={startStatus} quizData={quizData} />}
+      {quizEnd && <div className="score-container">
+        <p className="description">{`You scored ${correctCount}/5 correct answer.`}</p>
+        <button onClick={playAgain} className="start-btn check-btn">Play Again</button>
+      </div>}
+      {startStatus && <button onClick={checkAnswers} className="start-btn check-btn">Check Answer</button>}
+
     </div>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,16 +1,42 @@
+import { nanoid } from "nanoid";
+
 export default function Quiz(props) {
   const questionElements = props.quizData.map((quiz) => {
-    const allAnswers = quiz.incorrect_answers.concat(quiz.correct_answer);
+    
+    const allAnswers = quiz.randomAnswerPlacement
+    
     return (
-      <div className="question-box">
+      
+      <div key={nanoid()} className="question-box">
         <h3 className="question">{quiz.question}</h3>
         <div className="answers">
-          {allAnswers.map((answer) => {
-            return <button className="answer">{answer}</button>;
+          {props.startStatus ? (allAnswers.map((answer) => {
+            return <button id={quiz.questionId} key={nanoid()} onClick={props.selectAnswer} className={`answer ${quiz.selectedAnswer===answer ?"selected-answer": ""}`}>{answer}</button>;
+          })) :
+          allAnswers.map(answer=>
+          {
+            if(quiz.selectedAnswer === answer && quiz.rightAnswerSelected){
+            return <button id={quiz.questionId} key={nanoid()} onClick={props.selectAnswer} className="answer right-answer">{answer}</button>;
+            }
+
+            if(quiz.selectedAnswer === answer && !quiz.rightAnswerSelected){
+            return<button id={quiz.questionId} key={nanoid()} onClick={props.selectAnswer} className="answer wrong-answer">{answer}</button>;
+               
+            }
+            if(quiz.selectedAnswer !== answer && answer===quiz.correct_answer){
+            return <button id={quiz.questionId} key={nanoid()} onClick={props.selectAnswer} className="answer right-answer">{answer}</button>;
+            }
+
+            if(quiz.selectedAnswer !== answer){
+            return <button id={quiz.questionId} key={nanoid()} onClick={props.selectAnswer} className="answer unselected-answer">{answer}</button>;
+
+            }
           })}
         </div>
       </div>
     );
   });
-  return <div className="quiz-container">{questionElements}</div>;
+  
+  return <div className="quiz-container">{questionElements}
+  </div>;
 }
